2f7b39307c92b6420fe4999e4ff1ce1b1147b79d
Iván Calle <ivancalle@sipay.es>
2018-02-05 14:39:57 +0100

    [PSDK-50] fix transaction object

--- sipay/ecommerce/responses/transaction.py
+++ sipay/ecommerce/responses/transaction.py
@@ -18,9 +18,20 @@ class Transaction:
         self.internal_code = data['code']
         self.method_name = data['method_name']
         self.operation = data['operation']
-        self.amount = Amount(data['amount'], data['currency'])
+        self.amount = Amount(int(data['amount']), data['currency'])
         self.authorization_id = data['authorization_id']
         self.description = data['description']
         self.masked_card = data['masked_card']
         self.operation_name = data['operation_name']
         self.status = data['status']
+
+    def __str__(self):
+        """Cast to string."""
+        ret = ''
+        for k, v in self.__dict__.items():
+            if not k.startswith('_'):
+                ret += '{0}={1}, '.format(k, v)
+
+        ret = '<{0}({1})>'.format(type(self).__name__,ret[:-2])
+
+        return ret
