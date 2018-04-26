5d52195f5527895e2b607d67821e082ab4c932fd
Mikel Uranga <mikeluranga@sipay.es>
2018-04-26 18:47:25 +0200

    [PSDK-53] Travis and tox coverage implemented

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from pathlib import Path
-from setuptools import setup
-from setuptools import find_packages
 
-from pip.req import parse_requirements
-from pip.download import PipSession
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+from setuptools import find_packages
+from setuptools import setup
 
 
 def requirements(filename):
     """Parse requirements from requirements.txt."""
     path = str(Path(filename))
-    reqs = parse_requirements(path, session=PipSession())
+    reqs = parse_requirements(path, session=False)
     return [str(req.req) for req in reqs]
-
 
 setup(
     name='sipay',
