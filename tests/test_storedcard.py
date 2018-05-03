fde149e0ed4d6c7005c1f2b91b99e7f71850e16d
Mikel Uranga <mikeluranga@sipay.es>
2018-05-03 12:13:25 +0200

    [PSDK-61] StoredCard test with 100% coverage

---
+++ tests/test_storedcard.py
@@ -0,0 +1,26 @@
+import unittest
+from sipay.paymethod.storedcard import StoredCard
+
+
+class StoredCardTests(unittest.TestCase):
+
+    def setUp(self):
+        self.storedcard = StoredCard('token-card')
+
+    def test_init_storedcard(self):
+        with self.assertRaises(ValueError):
+            storedcard = StoredCard('token')
+        with self.assertRaises(TypeError):
+            storedcard = StoredCard(123)
+
+    def test_get_set_token(self):
+        self.assertEqual(self.storedcard.token, 'token-card')
+        self.storedcard.token = 'token-card-2'
+        self.assertEqual(self.storedcard.token, 'token-card-2')
+        with self.assertRaises(ValueError):
+            self.storedcard.token = 'token'
+        with self.assertRaises(TypeError):
+            self.storedcard.token = 123
+
+    def test_to_dict(self):
+        self.assertEqual(self.storedcard.to_dict(), {'token': 'token-card'})
