534728f4fb8560495f02a3aa1c4cf6de868d3f3f
Mikel Uranga <mikeluranga@sipay.es>
2018-05-03 09:44:17 +0200

    [PSDK-53] Amount class and Card class tests added

---
+++ tests/test_amount.py
@@ -0,0 +1,121 @@
+import unittest
+from sipay.amount import Amount
+
+
+class AmountTests(unittest.TestCase):
+
+    def setUp(self):
+        self.amount1 = Amount(100, 'EUR')
+        self.amount3 = Amount(100, 'EUR')
+        self.amount2 = Amount(200, 'EUR')
+        self.amount4 = Amount(300, 'TRY')
+
+    def test_init_amount(self):
+        with self.assertRaises(TypeError):
+            self.amount5 = Amount('300.00', 'USD', '#', '.')
+        with self.assertRaises(TypeError):
+            self.amount5 = Amount('300.00', 'USD', '.', '$')
+        with self.assertRaises(ValueError):
+            self.amount5 = Amount('300.00', 'UK', ',', '.')
+        with self.assertRaises(ValueError):
+            self.amount5 = Amount(300.00, 'UK', ',', '.')
+        with self.assertRaises(TypeError):
+            self.amount5 = Amount(300, 'USD', '#', '.')
+        with self.assertRaises(TypeError):
+            self.amount5 = Amount(300, 'USD', ',', '$')
+        with self.assertRaises(TypeError):
+            self.amount5 = Amount(300, 'USD', ',', ',')
+
+    def test_get_set_currency(self):
+        self.assertEqual(self.amount1.currency, 'EUR')
+        self.amount4.currency = 'TND'
+        self.assertEqual(self.amount4.currency, 'TND')
+        with self.assertRaises(ValueError):
+            self.amount4.currency = 'ASD'
+        with self.assertRaises(TypeError):
+            self.amount4.currency = 1
+
+    def test_get_set_amount(self):
+        self.assertEqual(self.amount1.amount, 100)
+        self.amount4.amount = 500
+        self.assertEqual(self.amount4.amount, 500)
+        with self.assertRaises(ValueError):
+            self.amount4.amount = -1
+        with self.assertRaises(TypeError):
+            self.amount4.amount = 'something'
+
+    def test_amount_gt_amount(self):
+        self.assertFalse(self.amount1 > self.amount2)
+        self.assertTrue(self.amount2 > self.amount1)
+        with self.assertRaises(TypeError):
+            self.amount1 > self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 > 'something'
+
+    def test_amount_lt_amount(self):
+        self.assertTrue(self.amount1 < self.amount2)
+        self.assertFalse(self.amount2 < self.amount1)
+        with self.assertRaises(TypeError):
+            self.amount1 < self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 < 'something'
+
+    def test_amount_add_amount(self):
+        result = self.amount1 + self.amount2
+        self.assertIsInstance(result, Amount)
+        self.assertEqual(result.amount, 300)
+        self.assertEqual(result.currency, 'EUR')
+        with self.assertRaises(TypeError):
+            self.amount1 + self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 + 'something'
+        with self.assertRaises(TypeError):
+            'something' + self.amount1
+
+    def test_amount_sub_amount(self):
+        result = self.amount2 - self.amount1
+        self.assertIsInstance(result, Amount)
+        self.assertEqual(result.amount, 100)
+        self.assertEqual(result.currency, 'EUR')
+        with self.assertRaises(ValueError):
+            self.amount1 - self.amount2
+        with self.assertRaises(TypeError):
+            self.amount1 - self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 - 'something'
+        with self.assertRaises(TypeError):
+            'something' - self.amount1
+
+    def test_amount_ge_amount(self):
+        self.assertTrue(self.amount2 >= self.amount1)
+        self.assertFalse(self.amount1 >= self.amount2)
+        self.assertTrue(self.amount1 >= self.amount3)
+        with self.assertRaises(TypeError):
+            self.amount1 >= self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 >= 'something'
+
+    def test_amount_le_amount(self):
+        self.assertFalse(self.amount2 <= self.amount1)
+        self.assertTrue(self.amount1 <= self.amount2)
+        self.assertTrue(self.amount1 <= self.amount3)
+        with self.assertRaises(TypeError):
+            self.amount1 <= self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 <= 'something'
+
+    def test_amount_eq_amount(self):
+        self.assertTrue(self.amount1 == self.amount3)
+        self.assertFalse(self.amount1 == self.amount2)
+        with self.assertRaises(TypeError):
+            self.amount1 == self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 == 'something'
+
+    def test_amount_uneq_amount(self):
+        self.assertTrue(self.amount2 != self.amount1)
+        self.assertFalse(self.amount1 != self.amount3)
+        with self.assertRaises(TypeError):
+            self.amount1 != self.amount4
+        with self.assertRaises(TypeError):
+            self.amount1 != 'something'
---
+++ tests/test_card.py
@@ -0,0 +1,52 @@
+import unittest
+from sipay.paymethod.card import Card
+
+
+class CardTests(unittest.TestCase):
+
+    def setUp(self):
+        self.card = Card('6712009000000205', 2050, 1)
+
+    def test_init_card(self):
+        with self.assertRaises(TypeError):
+            card = Card('6712009000000205')
+        with self.assertRaises(TypeError):
+            card = Card('671200900000020')
+        with self.assertRaises(TypeError):
+            card = Card('671200900000020W')
+        with self.assertRaises(TypeError):
+            card = Card('6712009000000205', 2050)
+        with self.assertRaises(TypeError):
+            card = Card('6712009000000205', 'something', 1)
+        with self.assertRaises(TypeError):
+            card = Card('6712009000000205', '', 1)
+        with self.assertRaises(TypeError):
+            card = Card('6712009000000205', 2050, '1')
+
+    def test_get_set_card(self):
+        self.assertEqual(self.card.card_number, '6712009000000205')
+        with self.assertRaises(TypeError):
+            self.card.card_number = 1
+        with self.assertRaises(ValueError):
+            self.card.card_number = '1'
+
+    def test_get_set_year_month(self):
+        self.assertEqual(self.card.year, 2050)
+        self.assertEqual(self.card.month, 1)
+        self.card.set_expiration_date(2049, 2)
+        self.assertEqual(self.card.year, 2049)
+        self.assertEqual(self.card.month, 2)
+        with self.assertRaises(ValueError):
+            self.card.set_expiration_date(998, 2)
+        with self.assertRaises(TypeError):
+            self.card.set_expiration_date(998.00, 2)
+        with self.assertRaises(ValueError):
+            self.card.set_expiration_date(1000, 13)
+        with self.assertRaises(TypeError):
+            self.card.set_expiration_date(1000, 2.00)
+        with self.assertRaises(Exception):
+            self.card.set_expiration_date(1999, 1)
+
+    def test_is_expired(self):
+        with self.assertRaises(Exception):
+            card = Card('6712009000000205', 2018, 3)
