888e212f135cc4a7e4413b51c99e25ec9a249916
Mikel Uranga <mikeluranga@sipay.es>
2018-05-03 16:58:09 +0200

    [PSDK-61] amount class fixed

--- sipay/amount.py
+++ sipay/amount.py
@@ -15,18 +15,18 @@ class Amount:
         self.separator = separator
         self.decimal_separator = decimal_separator
 
+        if not isinstance(separator, str) or \
+           separator not in THOUSAND_SEPARATORS:
+            raise TypeError('separator must be [ ,\'.].')
+
+        if not isinstance(decimal_separator, str) or \
+           decimal_separator not in DECIMAL_SEPARATORS:
+            raise TypeError('decimal_separator must be [,.]')
+
+        if decimal_separator == separator:
+            raise TypeError('separators are equals.')
+
         if isinstance(amount, str):
-            if not isinstance(separator, str) or \
-               separator not in THOUSAND_SEPARATORS:
-                raise TypeError('separator must be [ ,\'.].')
-
-            if not isinstance(decimal_separator, str) or \
-               decimal_separator not in DECIMAL_SEPARATORS:
-                raise TypeError('decimal_separator must be [,.]')
-
-            if decimal_separator == separator:
-                raise TypeError('separators are equals.')
-
             if len(separator) == 1:
                 separator = "\{}".format(separator)
 
@@ -160,11 +160,10 @@ class Amount:
         decimal = fmt_amount[-dec:]
         integer = fmt_amount[:-dec]
         length = len(integer)
-        init = length%3 if length%3 > 0 else 3;
+        init = length % 3 if length % 3 > 0 else 3
         integer_ftm = integer[:init]
 
         for i in range(init, length, 3):
             integer_ftm += "{0}{1}".format(self.separator, integer[i:i+3])
-
 
         return integer_ftm + self.decimal_separator + decimal + self.currency
